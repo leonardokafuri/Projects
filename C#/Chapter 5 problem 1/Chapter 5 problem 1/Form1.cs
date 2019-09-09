9558af7d69e21de5e2372d7821bac51bda68d028
Leonardo Kafuri <48772447+leonardokafuri@users.noreply.github.com>
2019-09-09 14:17:39 -0700

    Add files via upload

---
+++ C#/Chapter 5 problem 1/Chapter 5 problem 1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter_5_problem_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            int n;
+            int s;
+            s = int.Parse(textBox1.Text);  //speed
+            n = int.Parse(textBox2.Text);  //hours
+            for (int a =1; a<= n ; a++) 
+            {
+                richTextBox1.AppendText("After Hour "  + a + ". The distance is " + (a * s) + "\n");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+        }
+    }
+}
---
+++ C#/Problem 1/Problem 1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Problem_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str = textBox1.Text;
+            int n;
+            n=countMe(str);
+            label2.Text = "There are " + n + " words ";
+        }
+
+        private int countMe (string x)
+        {
+            int numOfwords = 0;
+            string[] t = x.Split(); // default is space 
+            numOfwords = t.Length;
+
+            return numOfwords;
+        }
+
+    }
+}
